95f5c6c3dc8486352921a240ea774be015d19240
Cosimo Leone <cosimo@stackmob.com>
2013-03-28 16:06:30 -0700

    Import SMPushRegisterDevice example by Sid (some fixes/modifications made) Util.java also by Sid Direct Push Notification done Broadcast Push Notification done

---
+++ java/src/main/java/com/stackmob/example/Util.java
@@ -0,0 +1,11 @@
+package com.stackmob.example;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stackmob
+ * Date: 3/28/13
+ * Time: 1:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Util {
+}
---
+++ java/src/main/java/com/stackmob/example/push/SMPushRegisterDevice.java
@@ -0,0 +1,11 @@
+package com.stackmob.example.push;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stackmob
+ * Date: 3/28/13
+ * Time: 12:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SMPushRegisterDevice {
+}
